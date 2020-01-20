bbf77de0389b3857be83119b1d2f90c28c1ec972
G Sriram <sgcuber24@gmail.com>
2020-01-20 13:40:39 +0530

    Passed categoryID and title top meals screen

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -5,14 +5,15 @@ import './category_meals_screen.dart';
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
+  final String id;
 
-  CategoryItem(this.title, this.color);
+  CategoryItem(this.title, this.color, this.id);
 
   void selectCategory(BuildContext ctx) {
     Navigator.of(ctx).push(
       MaterialPageRoute(
         builder: (_) {
-          return CategoryMealsScreen();
+          return CategoryMealsScreen(id, title);
         },
       ),
     );
